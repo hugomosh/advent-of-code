ca2af6cde6a1b39899ce68d920fdffb625599a59
hugomosh <hugomosh@gmail.com>
2021-12-06 20:57:15 -0800

    Day 6

--- ts/2021/06/index.ts
+++ ts/2021/06/index.ts
@@ -2,8 +2,8 @@ import getInput, { readTestInputFile } from "../../../utils/getInput";
 
 const problem = {
   year: 2021,
-  day: 4,
-  part1Done: false,
+  day: 6,
+  part1Done: true,
   part2Done: false,
 };
 
@@ -14,25 +14,68 @@ const part1 = () => {
 };
 
 const parseInput = (input: string) => {
-  return input.split("\n");
+  return input.split("\n")[0].split(",").map(Number);
 };
 
 function solvePart1(input: any): number {
   console.info(`Solving part 1. ${problem.year}/12/${problem.day}`);
-  const len = input[0].length;
+  let len = input.length;
   console.log({ len, input });
   let res = 987;
+  for (let i = 0; i < 80; i++) {
+    //console.log("day", i, input);
+
+    for (let j = 0; j < len; j++) {
+      const element = input[j];
+      input[j] -= 1;
+      if (input[j] < 0) {
+        input[j] = 6;
+        input.push(8);
+      }
+    }
+    len = input.length;
+  }
+  //console.log(input);
   //parseInt(gamma.join(""), 2)
 
-  return res;
+  return input.length;
 }
 
 function solvePart2(input: any): number {
-  const len = input[0].length;
-  console.log({ len, input });
-  let res2 = 123;
+  console.info(`Solving part 222. ${problem.year}/12/${problem.day}`);
+  let len = input.length;
+  console.log({ len, input }); /// I need a better structure
 
-  return res2;
+  let cuenta = new Map<number, number>();
+
+  for (let k = 0; k < input.length; k++) {
+    const element = input[k];
+    cuenta.set(element, 1 + (cuenta.get(element) ?? 0));
+  }
+
+  for (let i = 0; i < 256; i++) {
+    //console.log("day", i, input);
+    const newCuenta = new Map<number, number>();
+
+    for (let [a, b] of cuenta.entries()) {
+      //console.log(i, a, b);
+      if (a > 0) {
+        newCuenta.set(a - 1, cuenta.get(a)!);
+      } else {
+        newCuenta.set(8, b!);
+      }
+    }
+    newCuenta.set(6, (cuenta.get(0) ?? 0) + (newCuenta.get(6) ?? 0));
+    cuenta = newCuenta;
+  }
+  //console.log(cuenta);
+  //parseInt(gamma.join(""), 2)\
+  let res = 0;
+  for (let y of cuenta.values()) {
+    console.log({ y });
+    res += y;
+  }
+  return res;
 }
 
 const part2 = () => {
@@ -43,9 +86,9 @@ const part2 = () => {
 
 const testPart1 = (): boolean => {
   const input = readTestInputFile(problem.year, problem.day);
+  console.info("test 1");
   console.assert(input != "" && input.length > 0, "Empty test input part1 !");
-  if (input != "") return true; // Skip test and try problem
-  console.info("Running test 1");
+  if (input == "") return true; // Skip test and try problem
   const parsed = parseInput(input);
 
   const expected = 5934;
@@ -59,7 +102,7 @@ const testPart2 = () => {
   const input = readTestInputFile(problem.year, problem.day);
   const parsed = parseInput(input);
 
-  const expected = 230;
+  const expected = 26984457539;
   const actual = solvePart2(parsed);
   console.assert(actual === expected, `T1: ${actual} vs the expected: ${expected}`);
 };
@@ -69,10 +112,10 @@ function main() {
   console.log(`Running Advent of Code ${problem.year} Day: ${problem.day}`);
   if (!problem.part1Done) {
     if (testPart1()) {
-      //console.log(`Solution 1: ${part1()}`);
+      console.log(`Solution 1: ${part1()}`);
     }
   } else if (!problem.part2Done) {
-    testPart2();
+    //testPart2();
     console.log(`Solution 2: ${part2()}`);
   }
 }

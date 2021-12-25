226051fa7e75edc30ce60df4afce1da0d0dce46f
hugomosh <hugomosh@gmail.com>
2021-12-25 12:35:06 -0800

    Day 21 part 2

--- ts/2021/21/index.ts
+++ ts/2021/21/index.ts
@@ -5,15 +5,18 @@ const problem = {
   year: config.year,
   day: config.day,
   doTest: true,
-  expectedT1: 123,
-  expectedT2: 789,
-  part1Done: false,
+  expectedT1: 739785,
+  expectedT2: 444356092776315,
+  part1Done: 1,
   part2Done: false,
 };
 
 const parseInput = (input: string) => {
-  return input.split("\n");
-  // .filter(l=>l!="");
+  return input
+    .split("\n")
+    .filter((l) => l != "")
+    .map((l) => l.match(/\d+/g)![1])
+    .map(Number);
 };
 
 const part1 = () => {
@@ -30,17 +33,30 @@ const part2 = () => {
 
 function solvePart1(input: any): number {
   console.info(`Solving part 1. ${problem.year}/12/${problem.day}`);
-  const len = input.length;
-  console.info({ len, input });
-  let res = 987;
-  //parseInt(gamma.join(""), 2)
 
-  return res;
+  let isPlayer1Turn = true;
+  let scores = [0, 0];
+  let pos = input;
+  let dice = 1;
+  let diceRollCount = 0;
+  const goal = 1000;
+  while (scores[0] < goal && scores[1] < goal) {
+    const i = isPlayer1Turn ? 0 : 1;
+    pos[i] += dice + (dice + 1) + (dice + 2);
+    dice += 3;
+    diceRollCount += 3;
+    pos[i] = pos[i] % 10;
+    pos[i] = pos[i] == 0 ? 10 : pos[i];
+    scores[i] += pos[i];
+    isPlayer1Turn = !isPlayer1Turn;
+  }
+  console.log(diceRollCount, scores);
+
+  return scores.filter((x) => x < goal)[0] * diceRollCount;
 }
-
 /* ----------------------------   Part 2  ------------------------------*/
 function solvePart2(input: any): number {
-  const len = input[0].length;
+  const len = input.length;
   console.info({ len, input });
   let res2 = 123;
 
@@ -90,4 +106,25 @@ function main() {
   }
 }
 
+function getOptionForDie() {
+  const faces = [1, 2, 3];
+  const options = [],
+    frequencies = new Map<number, number>();
+  for (const f1 of faces) {
+    for (const f2 of faces) {
+      for (const f3 of faces) {
+        options.push([f1, f2, f3]);
+      }
+    }
+  }
+  const sum = options.map((r) => r.reduce((a, b) => a + b, 0));
+  console.log(sum);
+  for (const o of sum) {
+    frequencies.set(o, (frequencies.get(o) ?? 0) + 1);
+  }
+  console.log({ frequencies });
+  return frequencies;
+}
+
+getOptionForDie();
 main();

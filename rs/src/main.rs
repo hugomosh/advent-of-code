c2fa45a40fe0c626ed4e44cb6858d72cdd8b7cc3
Hugo Mosh Cardoza <hugomosh@gmail.com>
2022-01-15 01:54:12 +0000

    Start with rust.

---
+++ rs/src/main.rs
@@ -0,0 +1,9 @@
+mod y2015;
+
+fn main() {
+    println!("Hello, aoc!");
+
+    // let s = include_str!("input.txt");
+
+    y2015::d01::problem1();
+}
---
+++ rs/src/y2015/d01.rs
@@ -0,0 +1,6 @@
+pub fn problem1() {
+    println!("Aoc Year: 2015 Day: 01");
+
+    let s = std::fs::read_to_string("../input/2015/01.txt").unwrap();
+    dbg!(&s[..40]);
+}
